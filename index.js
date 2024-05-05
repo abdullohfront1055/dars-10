985332dc7416173c8946a5a944b3dc2c59c2795f
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-05 18:44:11 +0500

    tygadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let son1 = Number(prompt("Son kiriting"));
+let son2 = Number(prompt("Ikkincji son kiriting"));
+
+function kattami() {
+    if (son1 > son2) {
+        alert(son1 + " katta " + son2 + " dan");
+    } else {
+        alert(son1 + " Kichik " + son2 + " dan");
+    }
+}
+
